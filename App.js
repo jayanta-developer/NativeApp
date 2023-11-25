0408db5442a1a2cc82dd534d0da953e0c041aeb1
jayanta dey <116143724+jayanta-musquare@users.noreply.github.com>
2023-11-25 23:57:26 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  const [projectText, setProjectText] = useState("");
+  const [projectList, setProjectList] = useState([]);
+
+  const handelTextInput = (enteredText) => {
+    setProjectText(enteredText);
+  };
+
+  const handelBtnPress = () => {
+    setProjectList((privesVal) => [...privesVal, projectText]);
+    setProjectText("");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputBox}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your Project Name!"
+          value={projectText}
+          onChangeText={handelTextInput}
+        />
+        <Button onPress={handelBtnPress} title="Add Project" />
+      </View>
+      <View style={styles.listBox}>
+        {projectList &&
+          projectList.map((e, index) => (
+            <Text key={index}>
+              {index + 1}: {e}
+            </Text>
+          ))}
+      </View>
     </View>
   );
 }
@@ -13,8 +41,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 50,
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  inputBox: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderColor: "gray",
+    marginBottom: 15,
+  },
+  appBtn: {},
+  textInput: {
+    width: "90%",
+    borderWidth: 1,
+    padding: 7,
+    borderColor: "gray",
+    marginRight: 8,
+    borderRadius: 5,
+  },
+  listBox: {
+    flex: 7,
+    flexDirection: "column",
   },
 });
